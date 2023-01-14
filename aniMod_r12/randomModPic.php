5574695a1ca648bda938029839dbcf34d9b7451e
白子涵(秋夜Dx <20205900@stu.neu.edu.cn>
2023-01-14 22:02:45 +0800

    Add aniMod_r12

---
+++ aniMod_r12/randomModPic.php
@@ -0,0 +1,11 @@
+<?php
+//初始化随机数生成器种子，这行代码也可以删除
+$seed = time();
+//获取随机数
+$num = rand(1,10);
+//拼接图片地址
+// $picpath = "https://raw.githubusercontent.com/QiuYeDx/MyPicAPI/main/ani_r12/".$num.".jpeg";
+$picpath = "https://cdn.staticaly.com/gh/QiuYeDx/MyPicAPI@main/aniMod_r12/".$num.".jpeg";
+//重定位到图片
+die(header("Location: $picpath"));
+?>
