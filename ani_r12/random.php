f58f0ace8459fe6d447ae1d8168d3e1d9998f6a1
白子涵(秋夜Dx <20205900@stu.neu.edu.cn>
2023-01-14 21:37:22 +0800

    update ani_r12

--- ani_r12/random.php
+++ ani_r12/random.php
@@ -2,9 +2,10 @@
 //初始化随机数生成器种子，这行代码也可以删除
 $seed = time();
 //获取随机数
-$num = rand(1,13);
+$num = rand(1,14);
 //拼接图片地址
-$picpath = "https://raw.githubusercontent.com/QiuYeDx/MyPicAPI/main/ani_r12/".$num.".jpeg";
+// $picpath = "https://raw.githubusercontent.com/QiuYeDx/MyPicAPI/main/ani_r12/".$num.".jpeg";
+$picpath = "https://cdn.staticaly.com/gh/QiuYeDx/MyPicAPI@main/ani_r12/".$num.".jpeg";
 //重定位到图片
 die(header("Location: $picpath"));
 ?>
