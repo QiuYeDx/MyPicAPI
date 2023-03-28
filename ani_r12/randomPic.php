0f650e7da0e0d5f109330d1a015635d74340ef65
白子涵(秋夜Dx <71062076+QiuYeDx@users.noreply.github.com>
2023-03-28 22:06:44 +0800

    Add 20.jpeg

--- ani_r12/randomPic.php
+++ ani_r12/randomPic.php
@@ -2,7 +2,7 @@
 //初始化随机数生成器种子，这行代码也可以删除
 $seed = time();
 //获取随机数
-$num = rand(1,19);
+$num = rand(1,20);
 //拼接图片地址
 // $picpath = "https://raw.githubusercontent.com/QiuYeDx/MyPicAPI/main/ani_r12/".$num.".jpeg";
 $picpath = "https://cdn.staticaly.com/gh/QiuYeDx/MyPicAPI@main/ani_r12/".$num.".jpeg";
